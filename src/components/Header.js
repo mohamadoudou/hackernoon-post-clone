c5536a03e5f60609aa5154e39104a2d877dc60b2
mohamadou moctar <moctarm99@gmail.com>
2021-03-11 20:32:40 +0600

    build header component

---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { useEffect,useState } from 'react'
+import logo from '../images/logo.png'
+import mobileLogo from '../images/logo-mobile.png'
+import '../css/header.css'
+
+function Header(){
+    const [windowWidth, setWindowWidth] = useState(0)
+    const [breakPointWindow]=useState(790)
+    let resizeWindow = () => {
+        setWindowWidth(window.innerWidth)
+      }
+    
+    useEffect(() => {
+    resizeWindow()
+    window.addEventListener("resize", resizeWindow)
+    return () => window.removeEventListener("resize", resizeWindow)
+    }, []);
+
+    return(
+        <header className='header'>
+            {console.log('window size at any gevin moment',windowWidth,breakPointWindow)}
+            {/* <div className='header__container'> */}
+                <div className='upper__header'>
+                    <img src={windowWidth<=breakPointWindow?mobileLogo:logo} className='hackernoon__logo'/>
+                    <div className='upper__header__utils'>
+                        <form>
+                            <input type='text' placeholder='Search...'/>
+                        </form>
+                        { windowWidth<=breakPointWindow?null
+                           :<p>Start Writing</p>
+                        }
+                         { windowWidth<=breakPointWindow?null
+                           :<p>Log in</p>
+                        }
+                         { windowWidth<=breakPointWindow?<span>burg</span>
+                           :null
+                        }
+                    </div>
+                </div>
+                {windowWidth<=breakPointWindow?null:
+                 <div className='lower__header'>
+                    <a href='#'>Read</a>
+                    <a href='#'>Top Stories</a>
+                    <a href='#'>Write</a>
+                    <a href='#'>Listen</a>
+                    <a href='#'>Learn</a>
+                    <a href='#'>Advertise</a>
+                    <a href='#'>About</a>
+                    <a href='#'>Tech Companies</a>
+                    <a href='#'>Careers</a>
+                    <a href='#'>Slogging</a>
+                </div> }
+            {/* </div> */}
+        </header>
+    )
+}
+
+export default Header
